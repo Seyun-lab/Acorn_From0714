93ea31a86e1bcfd032c4220b8aecc248e31e0ade
Kim Gyu Taek <rlarbxor85@gs.anyang.ac.kr>
2025-07-24 12:40:15 +0900

    view에 db 내용 추가

--- mini_project/project_file/aiga/views.py
+++ mini_project/project_file/aiga/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render, redirect
 from django.db import connection
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.conf import settings  # settings.py에서 DB 정보 불러오기
+import MySQLdb  # mysqlclient 사용
+import json
 
 def index(request):
     return render(request, 'index.html')
@@ -29,3 +34,46 @@ def login(request):
 def in_notice(request):
     login_success = request.session.pop('login_success', False)
     return render(request, 'in_notice.html', {'login_success': login_success})
+  
+# DB 연결 함수 (mysqlclient)
+def get_connection():
+    db = settings.DATABASES['default']
+    return MySQLdb.connect(
+        user=db['USER'],
+        passwd=db['PASSWORD'],
+        host=db['HOST'],
+        port=int(db['PORT']),
+        db=db['NAME'],
+        charset='utf8'
+    )
+
+@csrf_exempt
+def save_data(request):
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        name = int(data.get('name'))
+        number = int(data.get('number'))
+        try:
+            conn = get_connection()
+            cur = conn.cursor()
+            # mysqlclient는 %s를 사용
+            cur.execute("INSERT INTO psy (name, number) VALUES (%s, %s)", (name, number))
+            conn.commit()
+            cur.close()
+            conn.close()
+            return JsonResponse({'status': 'ok'})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': str(e)})
+
+def get_data(request):
+    try:
+        conn = get_connection()
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM psy")
+        rows = cur.fetchall()
+        cur.close()
+        conn.close()
+        data = [{'name': r[0], 'number': r[1]} for r in rows]
+        return JsonResponse({'data': data})
+    except Exception as e:
+        return JsonResponse({'status': 'error', 'message': str(e)})  
